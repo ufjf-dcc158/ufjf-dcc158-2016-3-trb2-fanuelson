4ac12628904c2f366d26ea758a45846dae6de63e
fanuel nunes <fanuelson@gmail.com>
2016-12-07 22:12:36 -0200

    Registrar Vencedor/Atualizar Elo Feature: #3

--- app/controllers/partida.controllers.server.js
+++ app/controllers/partida.controllers.server.js
@@ -1,4 +1,6 @@
 var Partida = require('mongoose').model('Partida');
+var Jogador = require('mongoose').model('Jogador');
+var EloRating = require('elo-rating');
 
 module.exports.create = function(req, res, next){
     var part = new Partida(req.body);
@@ -10,6 +12,58 @@ module.exports.create = function(req, res, next){
         }
     });
 }
+
+
+
+module.exports.registrarVencedor = function (req, res, next) {
+
+    Partida.findById(req.body.idPartida, function (err, partida) {
+        if (err) next(err);
+        switch (req.body.numeroVencedor) {
+            case 1:
+                partida.idVencedor = partida.idJogador1;
+            case 2:
+                partida.idVencedor = partida.idJogador2;
+            default:
+                partida.idVencedor = null;
+        }
+
+        Partida.findByIdAndUpdate(partida._id, { $set: partida}, { new: true }, function (err, part2) {
+            if (err) next(err);
+            if(part2.idVencedor) {
+                var idVencedor = part2.idVencedor;
+                var idPerdedor = part2.idVencedor == part2.idJogador1 ? part2.idJogador2 : part2.idJogador1;
+            }else{
+                var idVencedor = part2.idJogador1;
+                var idPerdedor = part2.idJogador2;
+            }
+            Jogador.findById(idVencedor, function (err, jogadorVencedor) {
+                Jogador.findById(idPerdedor, function (err, jogadorPerdedor) {
+                    if(part2.idVencedor) {
+                        jogadorVencedor.qtdVitorias++;
+                        jogadorPerdedor.qtdDerrotas++;
+
+                        var resultElo = EloRating.calculate(jogadorVencedor.elo, jogadorPerdedor.elo, true);
+                        jogadorVencedor.elo = resultElo.playerRating;
+                        jogadorPerdedor.elo = resultElo.opponentRating;
+                    }else{
+                        jogadorVencedor.qtdEmpates++;
+                        jogadorPerdedor.qtdEmpates++;
+                    }
+                    Jogador.findByIdAndUpdate(jogadorVencedor._id, { $set: jogadorVencedor}, { new: true }, function (err, jv) {
+                        if (err) next(err);
+                        Jogador.findByIdAndUpdate(jogadorPerdedor._id, { $set: jogadorPerdedor}, { new: true }, function (err, jp) {
+                            if (err) next(err);
+                            res.send(part2);
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+}
+
 
 module.exports.list = function(req, res, next){
     Partida.find({}, function(err, partidas) {
